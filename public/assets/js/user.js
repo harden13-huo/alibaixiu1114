3e424345a846bec4cd0469e800008645f92228ba
chengmingmeng <1217259822@qq.com>
2019-11-14 17:06:30 +0800

    展示用户列表

--- public/assets/js/user.js
+++ public/assets/js/user.js
@@ -17,4 +17,42 @@ $('#userForm').on('submit',function () {
     });
     // 阻止表单默认行为
     return false
+});
+
+// 用户头像的提交
+$('#modifyBox').on('change','#avatar',function () {
+    // 用户选择到的文件
+    // this.files[0]
+
+    const formData = new FormData();
+    formData.append('avatar',this.files[0]);
+    $.ajax({
+        type: "post",
+        url: "/upload",
+        data: formData,
+        // 告诉ajax不要解析请求参数
+        processData: false,
+        // 告诉ajax不要设置请求参数的类型
+        contentType: false,
+        success: function (response) {
+            console.log(response)
+            // 实现头像预览
+            $('#preview').attr('src',response[0].avatar)
+            // 设置隐藏域的值 到时候点击提交时会发送给服务器
+            $('#hiddenAvatar').val(response[0].avatar)
+        }
+    });
+
 })
+
+// 展示用户列表
+$.ajax({
+    type: "get",
+    url: "/users",
+    success: function (response) {
+        console.log(response)
+        // 模板引擎字符串拼接
+        var html = template('userTpl',{data: response})
+        $('#userBox').html(html)
+    }
+});
