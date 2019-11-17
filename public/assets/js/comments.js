32a395883e91516ee87ef7e107aa68aebdb08c91
chengmingmeng <1217259822@qq.com>
2019-11-17 15:03:43 +0800

    评论批准删除完成

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -24,3 +24,36 @@ function changePage(page) {
         }
     });
 };
+
+// 点击批准评论状态
+$('#commentsBox').on('click','.status',function () {
+    // 获取被点击的状态
+    let status = $(this).attr('data-status')
+    // alert(status)   0  
+    let id = $(this).attr('data-id')
+    // alert(id)
+    $.ajax({
+        type: "put",
+        url: `/comments/${id}`,
+        data: {state: status == 0 ? 1 : 0},
+        success: function () {
+            location.reload()
+        }
+    });
+});
+
+// 删除评论
+$('#commentsBox').on('click','.delete',function () {
+    if (confirm('您确认要删除此评论吗？')) {
+        let id = $(this).attr('data-id')
+        // alert(id)
+        $.ajax({
+            type: "delete",
+            url: `/comments/${id}`,
+            success: function () {
+                location.reload()
+            }
+        });
+    };
+});
+
